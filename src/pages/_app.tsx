c0d04cf0ba15704e3900d997ff1952993691edc2
Junho Yeo <i@junho.io>
2024-02-14 06:24:25 +0900

    Add font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,22 @@
 import { AppProps } from 'next/app';
 import React from 'react';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '700', '800', '900'],
+});
 
 import { GlobalStyle } from '@/components/GlobalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <React.Fragment>
+    <main className={poppins.className}>
       <GlobalStyle />
       <Component {...pageProps} />
 
       <div id="portal" />
-    </React.Fragment>
+    </main>
   );
 }
 
