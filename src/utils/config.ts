2146f9ac331d5669ec74b1f0375bc41b7db18afb
joejoehoe <159974753+joejoehoe@users.noreply.github.com>
2024-02-14 18:45:14 +0900

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,3 @@
-export const CONTRACT_ADDRESS: `0x${string}` = '0x0'; // 여기에 컨트랙트 주소 입력
+export const FEE_SHARING_CONTRACT_ADDRESS: `0x${string}` = '0x129400290d9d20e2Ff951683DA847be7aeC57Be2'; // 여기에 컨트랙트 주소 입력
+export const CONTRACT_ADDRESS: `0x${string}` = '0x9d9d59842ddFF53fBb0241C25fAA2F439a4B2CE2'; // 여기에 컨트랙트 주소 입력
 // 예시: export const CONTRACT_ADDRESS: `0x${string}` = '0x6b175474e89094c44da98b954eedeac495271d0f';
